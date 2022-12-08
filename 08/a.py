4be7fe6627f75e4db8e63e9e9da2f4085f9f4e91
Alex Stephens <alexstephens9@gmail.com>
2022-12-08 20:39:04 +0000

    Day 8

---
+++ 08/a.py
@@ -0,0 +1,51 @@
+class Grid:
+    def __init__(self, grid):
+        self.grid = grid
+
+        self.rows = len(grid)
+        self.cols = len(grid[0])
+
+    def check_visible(self, i, j):
+        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        h = self.grid[i][j]
+
+        for d in dirs:
+            x = i + d[0]
+            y = j + d[1]
+            visible = True
+
+            while 0 <= x < self.rows and 0 <= y < self.cols:
+                if self.grid[x][y] >= h:
+                    visible = False
+                    break
+
+                x += d[0]
+                y += d[1]
+
+            if visible:
+                return True
+        return False
+
+    def count_visible(self):
+        count = 0
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.check_visible(i, j):
+                    count += 1
+        return count
+
+
+def main():
+    filename = "input.txt"
+
+    data = []
+    with open(filename) as f:
+        for line in f:
+            data.append(list(map(int, list(line.strip()))))
+
+    grid = Grid(data)
+    print(grid.count_visible())
+
+
+if __name__ == "__main__":
+    main()
